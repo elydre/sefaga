d02cd6ad68c5bfee0dec0d5b25c0f9c74f8cdd9b
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-15 15:04:06 +0200

    Create cli.py

---
+++ client/cli.py
@@ -0,0 +1,21 @@
+import json
+
+import ctools as ct
+from POOcom import ClientCom
+
+token = json.load(open(f"{ct.path}/token.json"))[input("username: ")]
+psw_key = ct.get_public_key(f"{ct.path}/keys/psw_public.pem")
+clt_key = ct.get_public_key(f"{ct.path}/keys/clt_public.pem")
+srv_key = ct.get_private_key(f"{ct.path}/keys/srv_private.pem")
+
+client = ClientCom(host="localhost", port=57321)
+
+client.send(ct.encrypt_string(token, psw_key))
+
+@client.on_message
+def on_message(data):
+    print(ct.decrypt_data(data, srv_key))
+
+while True:
+    msg = input("")
+    client.send(ct.encrypt_string(msg, clt_key))
