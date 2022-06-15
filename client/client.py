82bb47ba97d4f0d6bfa8ffe5b7ecaa9ef8777e8a
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-15 11:17:53 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,11 +1,11 @@
-import ctools as ct
-from POOcom import ClientCom
 import json
 
+import ctools as ct
+from POOcom import ClientCom
+
 token = json.load(open("token.json"))["token"]
+psw_key = ct.get_public_key(f"{ct.path}/keys/psw_public.pem")
 
 client = ClientCom(host="localhost", port=57321)
-
-psw_key = ct.get_public_key(f"{ct.path}/keys/psw_public.pem")
 
 client.send(ct.encrypt_string(token, psw_key))
