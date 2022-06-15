24baddf7216757f451a79570010e7dd4ed32dd70
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-15 15:37:04 +0200

    pre

--- server/server.py
+++ server/server.py
@@ -4,7 +4,6 @@ from _thread import start_new_thread
 
 import stools as st
 
-
 HOST = "localhost"      # server ip
 PORT = 57321            # server port
 
@@ -12,46 +11,70 @@ psw_key = st.get_private_key(f"{st.path}/keys/psw_private.pem")
 clt_key = st.get_private_key(f"{st.path}/keys/clt_private.pem")
 srv_key = st.get_public_key(f"{st.path}/keys/srv_public.pem")
 
-
 users = json.load(open(f"{st.path}/users.json"))
 
-online_users = []
+SD = {
+    "online_users": {},
+}
+
+def stop_user(conn: socket.socket) -> None:
+    print(f"{SD['online_users'][conn]['addr']} disconnected from socket")
+    del SD["online_users"][conn]
+    conn.close()
 
 def login_user(conn: socket.socket, addr: tuple) -> None:
-    print(f"{addr[0]}:{addr[1]} connected to socket")
+    str_addr = f"{addr[0]}:{addr[1]}"
 
-    data = conn.recv(1024)
-    if not data: return conn.close()
+    print(f"{str_addr} connected to socket")
+
+    SD["online_users"][conn] = {
+        "addr": str_addr,
+        "loged": False,
+        "name": None,
+        "plvl": None,
+        "conn": conn
+    }
+
+    try:
+        data = conn.recv(1024)
+        if not data: return conn.close()
+    except ConnectionResetError:
+        return stop_user(conn)
     
     dd = st.decrypt_data(data, psw_key)[0]
-    print(f"{addr[0]}:{addr[1]} token: {dd}")         # dev only, not safe
+    print(f"{str_addr} token: {dd}")        # dev only, not safe
     
     if user := st.check_user(dd, users):
-        if user["name"] in [u["name"] for u in online_users]:
-            print(f"{user['name']} is already online")
-            conn.send(f"{st.encrypt_string('server: user already online', srv_key)}<end>".encode())
-            return conn.close()
-        print(f"{addr[0]}:{addr[1]} user name: {user['name']}")
-        user["ip"], user["port"], user["conn"] = addr[0], addr[1], conn
-        user_com(user)
-        print(f"{addr[0]}:{addr[1]} logout")
+        if user["name"] in [u["name"] for u in SD["online_users"].values()]:
+            print(f"{user['name']} is already loged")
+            conn.send(f"{st.encrypt_string('server: user already loged', srv_key)}<end>".encode())
+            return stop_user(conn)
+        
+        print(f"{str_addr} user name: {user['name']}")
+        
+        SD["online_users"][conn]["name"] = user["name"]
+        SD["online_users"][conn]["plvl"] = user["plvl"]
+        SD["online_users"][conn]["loged"] = True
+
+        user_com(conn)
+        print(f"{str_addr} logout")
     else:
-        print(f"{addr[0]}:{addr[1]} invalid token")
-        return conn.close()
+        print(f"{str_addr} invalid token")
+        
+    return stop_user(conn)
 
 
-def user_com(user: dict) -> None:
-    online_users.append(user)
-    conn = user["conn"]
+def user_com(conn: socket.socket) -> None:
+    user_name = SD["online_users"][conn]["name"]
     while True:
         try:
             data = conn.recv(1024)
             if not data: break
             dd = st.decrypt_data(data, clt_key)[0]
-            msg = f"{user['name']}: {dd}"
+            msg = f"{user_name}: {dd}"
             print(msg)
-            for u in online_users:
-                if u["name"] != user["name"]:
+            for u in SD["loged_users"]:
+                if u["name"] != user_name:
                     u["conn"].send(f"{st.encrypt_string(msg, srv_key)}<end>".encode())
         except ConnectionResetError:
             break
